19d0c4d571f98a31dca9bb74b5134c3ab5b9bc50
yankul <yankul.victor@i.ua>
2025-05-03 07:24:55 +0300

    created header, installed fonts

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss_css      = require('gulp-sass')(require('sass'));
+const concat        = require('gulp-concat');
+const uglify        = require('gulp-uglify');
+const browserSync   = require('browser-sync').create();
+const imagemin      = require('gulp-imagemin-fix');
+const del           = require('del');
+
+
+function styles() {
+  return src('app/scss/**/*.scss')
+    .pipe(scss_css({ outputStyle: 'compressed' }).on("error", scss_css.logError))
+    .pipe(concat('style.min.css'))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
+}
+
+function scripts() {
+  return src([
+    'app/js/main.js',
+    'node_modules/jquery/dist/jquery.js'
+  ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
+}
+
+function images() {
+  return src('app/images/**/*.*')
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(dest('dist/images'));
+}
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: './app'
+    }
+  })
+}
+
+function build() {
+  return src([
+    'app/**/*.html',
+    'app/css/style.min.css',
+    'app/js/main.min.js'
+  ], { base: 'app' })
+  .pipe(dest('dist'))
+}
+
+function cleanDist() {
+  return del('dist')
+}
+
+function watching() {
+  watch(['app/scss/**/*.scss'], styles)
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/**/*.html']).on('change', browserSync.reload)
+}
+
+exports.build = series(cleanDist, images, build)
+exports.default = parallel(styles, scripts, browsersync, watching);
